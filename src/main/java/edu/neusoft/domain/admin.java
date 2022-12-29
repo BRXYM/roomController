4200d96bb6db93649b26af3f29b25caebd86a7cf
本人想养喵 <2637143104@qq.com>
2022-12-29 10:56:26 +0800

    Revert "新增遗失接口"
    This reverts commit 4835ec00268db8091278e5e64506281b7653686d.

---
+++ src/main/java/edu/neusoft/domain/admin.java
@@ -0,0 +1,5 @@
+package edu.neusoft.domain;
+
+public class admin {
+
+}
--- src/main/java/edu/neusoft/service/AdminService.java
+++
@@ -1,9 +0,0 @@
-package edu.neusoft.service;
-
-import edu.neusoft.domain.Admin;
-import edu.neusoft.utils.AdminResult;
-
-public interface AdminService {
-    AdminResult AdminloginService(int admin_id, String admin_password);
-    AdminResult AdminupdateService(Admin admin);
-}
